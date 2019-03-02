ac93d82c47109a3e1110d0cc42ffa3a78f6b7ec8
NeeX <47035947+NeeX09@users.noreply.github.com>
2019-03-02 19:33:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,31 @@
-cconst Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-    const neextoken = "NDIzODg5Njk3MjE2MDA0MTA4.DvxQDA.OYlWq5jEl68vFoeIWNwHvdW2Skg"
-      neex.on('ready', () => {
-        neex.user.setGame(`GangUpForever`,'https://www.twitch.tv/TEST-Broadcast');
-          console.log('Im Ready!');
-  
-        });
-
-  neex.on('message', message => {
-    if (message.content.split(' ')[0] == '%k')
-       message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-
-           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                      message.delete();
-            
-                                                    });
-            
-                                                  });
-   neex.on("message", message => {
-       var prefix = "$";
- 
-             var args = message.content.substring(prefix.length).split(" ");
-                if (message.content.startsWith(prefix + "b")) {
-                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-                          if (!args[1]) {
-                            
-                                 let embed3 = new Discord.RichEmbed()
-                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
-                                       .setColor("#FF00FF")
-                                          message.channel.sendEmbed(embed3);
-                            
-                                        } else {
-
-                            
-                                           let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
-                                                                .setColor("#99999")
-                               
-                                                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
+client.on('ready', () => {
+  client.user.setGame(`GangUpForever | %help | By NeeX^ .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
+client.on('message', message => {
+   let embed = new Discord.RichEmbed()
 
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+if(message.content.split(' ')[0] == '%bc') {
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
+        message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+.addField('**● SENDER  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
+            .addField('***● SERVER  :***', `*** → ${message.guild.name}***`)               
+    .setColor('#ff0000')
+                 .addField('ّ', args)
+            m.send(``,{embed: bc});
+        });
+    }
+})
 client.login(process.env.BOT_TOKEN);

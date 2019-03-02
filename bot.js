41c6ce36f6828dc3cdad4d0b277d1eabc1ff94b9
NeeX <47035947+NeeX09@users.noreply.github.com>
2019-03-02 13:40:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,81 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
-const config = require('./config.json');
-const prefixbc = config.prefix;
-client.on('message', async message => {  
-if(message.author.bot) return;
-if(message.channel.type === 'dm') return;
-if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return;
-const args = message.content.split(" ").slice(1);
-if(message.content.startsWith(prefixbc + 'obc')) {  
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${message.guild.memberCount}__ مشترك**`);
-message.guild.members.forEach(m => { 
-m.send(args.replace('[user]', m));
-});}
-if(message.content.startsWith(prefixbc + 'bc')) {  
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى ${message.guild.members.filter(m => m.presence.status !== 'online').size}__ مشترك**`);
-message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-m.send(args.replace('[user]', m));
-});}
-if(message.content.startsWith(prefixbc + 'obc')) {  
-var role = message.mentions.roles.first();
-if(!role) {message.reply("لا توجد رتبة بهذا الاسم");return;}
-if(!args[0]) {
-message.channel.send(`قم بمنشنة الرتبة  | ${prefixbc}bcrole @admin message`);
-return;}
-message.channel.send(`**:loudspeaker:  تم ارسال هذة الرسالة الى __${role.members.size}__ مشترك**`);
-message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
-n.send(args[1].replace('[user]', n));});}
-if(message.content.startsWith(prefixbc + "help")) {
-const embed = new Discord.RichEmbed() .setColor("RANDOM").setDescription(`**${prefixbc}obc ⇏ لإرسال رسالة إلى جميع أعضاء السيرفر
-${prefixbc}bc ⇏ لإرسال رسالة إلى الأعضاء الأونلاين فقط
-${prefixbc}bcrole ⇏  لإرسآل رسالة لرتبة محدده 
-ex :   ${prefixbc}bcrole @admin message
-__تنبية__ : إذا أردت أن تمنشن العضو فقط أكتب بالرسالة
-\`[user]\` وسيقوم بإستبدالها بمنشن العضو**`);
-message.channel.sendEmbed(embed)}
+client.on('message',async message => {
+    if (message.content.startsWith('#js')) {
+  if(!message.channel.guild) return message.reply('This Command For Servers Only !');
+  let staff = message.guild.member(message.author).roles.find('name' , 'Coder');// رتبه السبورت
+  if(!staff) return msg.reply('🛑 You Cant Do This Command')
+      let jscodes = message.guild.channels.find(`name`, "discord-js");// روم يلي ينشر الكود فيه
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+  
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        let boi;
+        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
+  
+            message.channel.awaitMessages(filter, {
+              max: 1,
+              time: 90000,
+              errors: ['time']
+            })
+            .then(collected => {
+              collected.first().delete();
+              boi = collected.first().content;
+              let boi2;
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+  
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 90000,
+                  errors: ['time']
+                })
+                .then(collected => {
+                  collected.first().delete();
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**${message.guild.name}© :arrow_down:**            
+\`\`\`js
+${thisMessage}\`\`\`
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
+          }
+        }
+    );
 });
-
-   client.login(process.env.BOT_TOKEN); 
+      });
+    }
+      );
+    });
+}
+);
+      })}});
+client.login("process.env.BOT_TOKEN");

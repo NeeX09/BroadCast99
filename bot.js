d7369aedb3cd5bb2641a6974c492586beba4d76c
NeeX <47035947+NeeX09@users.noreply.github.com>
2019-03-04 17:25:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |Gang Up Broadcast`,"http://twitch.tv/Death Shop")
+client.user.setGame(`=bc |GangUpForever <3`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
